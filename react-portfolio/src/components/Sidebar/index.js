e1ebe4125f5c8f74ec910937d3c23a480c9f007d
Joshua_Muniz <joshua.mun97@gmail.com>
2023-04-10 12:14:19 -0700

    Updated Layout and added Home component

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,13 +1,14 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import JMDevs from '../../assets/images/JMDevs.png'
+import jmdev from '../../assets/images/jmdev.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img src={JMDevs} alt="logo" />
+            <img src={jmdev} alt="logo" />
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
@@ -20,6 +21,18 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/joshua-muniz-b8491025b/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Joshuamun97'>
+                    <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+                </a>
+            </li>
+        </ul>
 
     </div>
 )
