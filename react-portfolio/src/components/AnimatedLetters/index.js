4e4a584a96927088772b0306f942423cef0f5a9e
Joshua_Muniz <joshua.mun97@gmail.com>
2023-04-12 18:31:35 -0700

    Update home page and started working on logo

--- react-portfolio/src/components/AnimatedLetters/index.js
+++ react-portfolio/src/components/AnimatedLetters/index.js
@@ -13,4 +13,6 @@ const AnimatedLetters = ({letterClass, strArray, idx}) => {
             }
         </span>
     )
-}
+};
+
+export default AnimatedLetters
--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
+import { useEffect, useState } from "react";
+import Logo from "./Logo";
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const portfolioArray = ['A', 'n', ' ' ,'I', 'n', 't', 'e', 'r', 'a', 'c', 't', 'i', 'v', 'e', ' ','P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']
 
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4500)
+    }, [])
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>JM Developments <br /> An Interactive Portfolio by Joshua Muniz  </h1>
+                <h1>JM Developments <br /> <AnimatedLetters letterClass ={letterClass} strArray={portfolioArray} idx={15}/> <br /> By Joshua Muniz  </h1>
                 <h2>Full Stack Developer / JavaScript / Freelancer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
+            <Logo />
         </div>
     );
 }
