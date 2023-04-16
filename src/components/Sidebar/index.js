8d30d31ac082317d63531a2e4377381b01a37cc5
Joshua_Muniz <joshua.mun97@gmail.com>
2023-04-16 13:43:12 -0700

    Updated routes and moved layout component out of routes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,17 +7,17 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/REACT_Portfolio'>
+        <Link className='logo' to='/'>
             <img src={jmdev} alt="logo" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/REACT_Portfolio">
+            <NavLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/REACT_Portfolio/about">
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/REACT_Portfolio/contact">
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
