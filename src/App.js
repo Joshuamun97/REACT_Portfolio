5383ede800aa1fc2a56cf7d314689a17d8c12276
Joshua_Muniz <joshua.mun97@gmail.com>
2023-04-16 18:41:13 -0700

    Trying to fix routes bug

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ export default function App() {
     return (
         <>
             <Routes>
-            <Route element={<Layout />}>
-                    <Route index element={<Home />} />
+                <Route element={<Layout />}>
+                    <Route index element={<Navigate to="/home" />} />
+                    <Route path="home" element={<Home />} />
                     <Route path="about" element={<About />} />
                     <Route path="contact" element={<Contact />} />
-                    </Route>
+                </Route>
             </Routes>;
         </>
     );
